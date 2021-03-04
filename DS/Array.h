bba17f4610b7897d8b9a635a5f0660a6a33b7015
White777888 <levbelov2000@yandex.ru>
2021-03-04 23:59:41 +0300

    Added MergeSort, InsertSort, SubArray

--- DS/Array.h
+++ DS/Array.h
@@ -22,6 +22,11 @@ namespace MyLib {
 		//Функция для сортировки вставками
 		void insertion_sort(AType *arr, long left, long right, bool(*CompareFunc)(AType a, AType b) = AscOrd);
 
+		//Функция для сортировки слиянием
+		void merge_sort(AType *arr, long left, long right, bool(*CompareFunc)(AType a, AType b) = AscOrd);
+		//Функция, которая будет непосредственно сливать два массива
+		void merge(AType *arr, long left, long middle, long right, bool(*CompareFunc)(AType a, AType b) = AscOrd);
+
 	public:
 
 		//Конструкторы
@@ -99,9 +104,14 @@ namespace MyLib {
 		/*Основную работу будет выполнять функция
 		сортировки слиянием, но на небольших размерах массива
 		будет работать функция сортировки вставками*/
+		
+		//Сортировка вставками
 		void InsertSort(bool(*CompareFunc)(AType a, AType b) = AscOrd);
+		//Сортировка слиянием
+		void MergeSort(bool(*CompareFunc)(AType a, AType b) = AscOrd);
 		
-		
+		//Получение подмассива
+		Array SubArray(long left, long right);
 		
 	};
 
@@ -310,32 +320,119 @@ namespace MyLib {
 		return A;
 	}
 
+	//Функция сортировки слиянием
+	template<typename AType>
+	void Array<AType>::MergeSort(bool(*CompareFunc)(AType a, AType b))
+	{
+		merge_sort(arr, 0, length - 1, CompareFunc);
+	}
+	
+	//Функция возвращает подмассив
+	template<typename AType>
+	Array<AType> Array<AType>::SubArray(long left, long right)
+	{
+		//Проверка на адекватность границ
+		if (left < 0 || right >= length) {
+			throw Exception("Incorrect borders");
+		}
+		if (left > right) {
+			throw Exception("left > right!");
+		}
+
+		//Массив соответствующего размера
+		Array temp(right - left + 1);
+
+		for (int i = 0; i < temp.length; i++) {
+			temp[i] = this->arr[left + i];
+		}
+
+		return temp;
+	}
+	//Функция разбиения массива и последующего слияния
+	template<typename AType>
+	void Array<AType>::merge_sort(AType * arr, long left, long right, bool(*CompareFunc)(AType a, AType b))
+	{
+		if ((right - left + 1) <= 15) {
+			insertion_sort(arr, left, right, CompareFunc);
+		}
+		else if (left < right) {
+
+			//Нашли середину массива
+			long middle = (left + right) / 2;
+
+			//Рекурсивный вызов
+			//1
+			merge_sort(arr, left, middle, CompareFunc);
+			//2
+			merge_sort(arr, middle + 1, right, CompareFunc);
+
+			//Слияние подмассивов
+			merge(arr, left, middle, right, CompareFunc);
+		}
+	}
+	//Функция слияния двух массивов
+	template<typename AType>
+	void Array<AType>::merge(
+		AType * arr, long left, long middle, long right, bool(*CompareFunc)(AType a, AType b)){
+		//Размеры подмассивов
+		long s1 = middle - left + 1;
+		long s2 = right - middle;
+
+		//Динамические массивы
+		AType *a1 = new AType[s1];
+		AType *a2 = new AType[s2];
+
+		//Копируем данные из arr
+		for (long i = 0; i < s1; i++) {
+			a1[i] = arr[left + i];
+		}
+		for (long i = 0; i < s2; i++) {
+			a2[i] = arr[(middle + 1) + i];
+		}
+
+		//Указатели на следующие доставаемые элементы массивов
+		long na1 = 0, na2 = 0;
+
+		for (long i = left; i <= right; i++) {
+
+			//Если первый подмассив закончился
+			//тогда копипуем весь второй и уходим
+			if (na1 == s1) {
+				for (; i <= right; i++) {
+					arr[i] = a2[na2++];
+				}
+				break;
+			}
+			//Если второй подмассив закончился
+			//тогда копипуем весь первый и уходим
+			if (na2 == s2) {
+				for (; i <= right; i++) {
+					arr[i] = a1[na1++];
+				}
+				break;
+			}
+
+			//Записываем в выходную последовательность
+			//меньшее значение
+			if (CompareFunc(a2[na2], a1[na1])) {
+				arr[i] = a1[na1++];
+			}
+			else {
+				arr[i] = a2[na2++];
+			}
+		}
+
+		//Очищаем память для массивов
+		delete[]a1;
+		delete[]a2;
+
+	}
+
 	//Функция сортировки вставками
 	template<typename AType>
 	void Array<AType>::InsertSort(bool(*CompareFunc)(AType a, AType b))
 	{
 		insertion_sort(arr, 0, length - 1, CompareFunc);
-
-		//AType key;
-		//long j;
-
-		//for (long i = 1; i < length; i++) {
-		//	
-		//	//Запомнили элемент
-		//	key = arr[i];
-
-		//	//Установили счетчик
-		//	j = i - 1;
-
-		//	//Начинаем смещение элементов слева направо
-		//	while(j >= 0 && CompareFunc(arr[j], key)) {	
-		//		//Если элемент key должен быть левее/правее arr[j]
-		//		arr[j + 1] = arr[j];
-		//		j--;
-		//	}
-
-		//	arr[j+1] = key;
-		//}
 	}
 	template<typename AType> void Array<AType>::insertion_sort(AType *arr, long left, long right, bool(*CompareFunc)(AType a, AType b)) {
 
